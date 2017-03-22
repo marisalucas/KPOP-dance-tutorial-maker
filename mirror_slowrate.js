3e6f8822447f40cb47c713412d78bc62d7588e3f
marisalucas <mlucper@gmail.com>
2017-03-22 13:42:23 +0100

    Create mirror_slowrate.js

---
+++ mirror_slowrate.js
@@ -0,0 +1,35 @@
+//Toggle:
+(function(){
+  var node = document.createElement('style');
+  document.body.appendChild(node);
+  window.addStyleString = function(str) {
+    node.innerHTML = str;
+  }
+
+  cssText = ".xflip { \
+      -moz-transform: scale(-1, 1) !important;\
+      -webkit-transform: scale(-1, 1) !important;\
+      -o-transform: scale(-1, 1) !important;\
+      transform: scale(-1, 1) !important;\
+      filter: FlipH !important;\
+    }";
+
+  addStyleString(cssText);
+
+  var numberOfVideos = document.getElementsByTagName("video").length;
+
+  if(numberOfVideos >= 1)
+  {
+    var video;
+    for(var i = 0; i < numberOfVideos; ++i)
+    {
+        if(document.getElementsByTagName("video")[i].videoHeight > 100)
+      {
+          video = document.getElementsByTagName("video")[i];
+		  video.defaultPlaybackRate = 0.5;
+          video.classList.toggle('xflip');
+        break;
+      }
+    }
+  };
+})();
